8593983f49b0c401b6e3d6e40fb79ce5a88d6463
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-29 12:59:08 -0400

    fix: allowed custom properties to be passed to icons

--- src/components/full-nav.tsx
+++ src/components/full-nav.tsx
@@ -1,19 +1,13 @@
 import Link from 'next/link';
 import React from 'react'
-import Image from 'next/image';
+import { Icons } from './icons';
 
 export function FullNav() {
   return (
     <header className='w-screen z-40'>
         <nav className='px-4 md:px-0 flex justify-between h-20 items-center py-6 container max-w-[1200px] mx-auto'>
             <Link href={'/'}>
-              <Image 
-                src="/logo.svg" 
-                alt="Logo" 
-                width={150} 
-                height={30} 
-                priority 
-              />
+              <Icons.logo width="150" height="30"/>
             </Link>
             <Link className="px-4 py-2 rounded-full bg-slate-900 text-white text-sm" href={"/login"}>Get Started</Link>
         </nav>
