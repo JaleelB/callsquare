68f2d928e81ff2e066e16cafe1ec6ba119d94418
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-28 21:57:13 -0400

    feat: index route and layout

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function IndexPage(){
+    return(
+        <div>
+            <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+                <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+                    <h1 className="text-4xl md:text-6xl font-bold leading-tight">
+                        HI
+                    </h1>
+                </div>
+            </section>
+        </div>
+    )
+}
