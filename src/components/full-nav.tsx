bef051d28a26cc84216f73a9eaf4b3061dee84c9
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-28 21:56:49 -0400

    feat: nav bar implementation

---
+++ src/components/full-nav.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React from 'react'
+import Image from 'next/image';
+
+export function FullNav() {
+  return (
+    <header className='w-screen z-40'>
+        <nav className='px-4 md:px-8 flex justify-between h-20 items-center py-6 container max-w-[1200px] mx-auto'>
+            <Link href={'/'}>
+              <Image 
+                src="/logo.svg" 
+                alt="Logo" 
+                width={150} 
+                height={30} 
+                priority 
+              />
+            </Link>
+            <Link className="px-4 py-2 rounded-full bg-black text-white text-sm" href={"/login"}>Get Started</Link>
+        </nav>
+    </header>
+  );
+}
