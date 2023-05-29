755fa3a1b64de26c8e976ba9775c7ef82a97020b
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-28 21:55:23 -0400

    feat: updated glabl background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='bg-zinc-50'>{children}</body>
     </html>
   );
 }
