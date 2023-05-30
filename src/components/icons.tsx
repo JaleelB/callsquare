4d2771e4ff81d7515ec71a3188e74fab92ce1af2
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-05-29 23:46:21 -0400

    feat: added loading state and sign-in functionality to socialauthform

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -78,5 +78,23 @@ export const Icons = {
                 <polygon points="0 0 18 0 18 18 0 18"></polygon>
             </g>
         </svg>
+    ),
+    spinner: ({color, ..._props}: SVGProps<SVGSVGElement>) => (
+        <svg {..._props} viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke={color || "#0F172A"}>
+            <g fill="none" fillRule="evenodd">
+                <g transform="translate(1 1)" strokeWidth="2">
+                    <circle strokeOpacity=".5" cx="18" cy="18" r="18"/>
+                    <path d="M36 18c0-9.94-8.06-18-18-18">
+                        <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            from="0 18 18"
+                            to="360 18 18"
+                            dur="1s"
+                            repeatCount="indefinite"/>
+                    </path>
+                </g>
+            </g>
+        </svg>
     )
 }
--- src/components/social-auth-form.tsx
+++ src/components/social-auth-form.tsx
@@ -1,19 +1,59 @@
-import React from 'react'
+"use client"
+import * as React from "react"
+import { signIn } from "next-auth/react"
 import { Icons } from './icons'
 
 export default function SocialAuthForm (){
+
+  const [isGithubSocialLoading, setIisGithubSocialLoading] = React.useState<boolean>(false)
+  const [isGoogleSocialLoading, setIsGoogleSocialLoading] = React.useState<boolean>(false)
+  const [isDiscordSocialLoading, setIsDiscordSocialLoading] = React.useState<boolean>(false)
+
   return (
     <section className='w-full mx-auto flex flex-col gap-1'>
-        <button className='w-full h-12 bg-slate-900 flex gap-3 text-white justify-center items-center rounded-sm'>
-            <Icons.github color='white' width="16" height="16"/>
+        <button 
+          className='w-full h-12 bg-slate-900 flex gap-3 text-sm text-white justify-center items-center rounded-sm'
+          onClick={() => {
+            setIisGithubSocialLoading(true)
+            signIn("github")
+          }}
+          disabled={isGithubSocialLoading}
+        >
+            {isGithubSocialLoading ? (
+              <Icons.spinner  width="16" height="16" color="white"/>
+            ) : (
+              <Icons.github color='white' width="16" height="16"/>
+            )}
             Continue with Github
         </button>
-        <button className='w-full h-12 border-2 border-slate-200 text-slate-900 flex gap-3 justify-center items-center rounded-sm'>
-            <Icons.google color='#0f172a' width="16" height="16"/>
+        <button 
+          className='w-full h-12 border-2 border-slate-200 text-sm text-slate-900 flex gap-3 justify-center items-center rounded-sm'
+          onClick={() => {
+            setIsGoogleSocialLoading(true)
+            signIn("google")
+          }}
+          disabled={isGoogleSocialLoading}
+        >
+            {isGoogleSocialLoading ? (
+              <Icons.spinner  width="16" height="16"/>
+            ) : (
+              <Icons.google color='#0f172a' width="16" height="16"/>
+            )}
             Continue with Google
         </button>
-        <button className='w-full h-12 border-2 border-slate-200 text-slate-900 flex gap-3 justify-center items-center rounded-sm'>
-            <Icons.discord color='#0f172a' width="16" height="16"/>
+        <button 
+          className='w-full h-12 border-2 border-slate-200 text-slate-900 text-sm flex gap-3 justify-center items-center rounded-sm'
+          onClick={() => {
+            setIsDiscordSocialLoading(true)
+            signIn("discord")
+          }}
+          disabled={isDiscordSocialLoading}
+        >
+            {isDiscordSocialLoading ? (
+              <Icons.spinner  width="16" height="16"/>
+            ) : (
+              <Icons.discord color='#0f172a' width="16" height="16"/>
+            )}
             Continue with Discord
         </button>
     </section>
