7a95a2067f22e5e985b7cbd0f1161789d2ace6b5
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-01 23:30:31 -0400

    feat: display badge

--- src/app/(calls)/calls/page.tsx
+++ src/app/(calls)/calls/page.tsx
@@ -1,10 +1,16 @@
+import Badge from "~/components/ui/badge";
+import { formatDate } from "~/lib/date";
+
 export default function CallsPage(){
     return (
         <section className="container max-w-[1400px] space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 mx-auto">
             <div className="flex flex-col items-center gap-4 text-center">
-                <h1 className="text-4xl text-slate-900 md:text-6xl font-bold leading-tight">
-                    Welcome to Callsquare
-                </h1>
+                <div>
+                    <Badge date={formatDate(new Date())}/>
+                    <h1 className="mt-4 text-4xl text-slate-900 md:text-6xl font-bold leading-tight">
+                        Welcome to Callsquare
+                    </h1>
+                </div>
             </div>
         </section>
     )
