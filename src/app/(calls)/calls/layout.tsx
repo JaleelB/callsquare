4ba20afc84f0ae81265d8ea9048a7e43fe2878aa
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-02 20:43:21 -0400

    feat: retrieve current user session

--- src/app/(calls)/calls/layout.tsx
+++ src/app/(calls)/calls/layout.tsx
@@ -1,20 +1,30 @@
 import SiteFooter from "~/components/footer";
 import FullNav from "~/components/full-nav";
+import UserAccountDropdown from "~/components/user-account-dropdown";
+import { getCurrentUser } from "~/lib/session";
 
-export default function HomePageLayout({
+export default async function HomePageLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+
+  const user = await getCurrentUser()
+
   return (
     <div className="flex min-h-screen flex-col">
-        <FullNav>
-          User
-        </FullNav>
-        <main className="flex-1 w-screen flex items-center">
-            {children}
-        </main>
-        <SiteFooter/>
+      <FullNav>
+        <UserAccountDropdown user={{ 
+            name: user?.name || '', 
+            email: user?.email || '',
+            image: user?.image || '' 
+          }} 
+        />
+      </FullNav>
+      <main className="flex-1 w-screen flex items-center">
+          {children}
+      </main>
+      <SiteFooter/>
     </div>
   );
 }
--- src/components/user-account-dropdown.tsx
+++ src/components/user-account-dropdown.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react';
 import { signOut } from 'next-auth/react';
 import ToastContext from '~/context/toast-context';
