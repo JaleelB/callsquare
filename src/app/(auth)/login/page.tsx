38365f8b07dc87d9dbc490347602c05e6fba4e01
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-03 01:34:27 -0400

    fix: improved navigation and metadata for auth pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,15 +2,24 @@ import { type Metadata } from "next"
 import Link from "next/link"
 import { Icons } from "~/components/ui/icons"
 import SocialAuthForm from "~/components/social-auth-form"
+import Button from "~/components/ui/button"
 
 export const metadata: Metadata = {
-  title: "Login",
-  description: "Login to your CallSquare account",
+  title: "CallSquare - Connect with Ease",
+  description: "Sign in to CallSquare and start connecting with your friends, family, and colleagues through seamless video calls.",
 }
 
 export default function LoginPage() {
   return (
     <main className="container flex h-screen w-screen mx-auto flex-col items-center justify-center">
+      <Button variant="transparent" className="absolute top-4 left-4 md:left-8">
+        <Link href="/" >
+          <span className="flex">
+            <Icons.chevronLeft className="mr-2" width={16} height={16} />
+            Go back
+          </span>
+        </Link>
+      </Button>
       <div className="mx-auto -mt-16 flex flex-col justify-center gap-8 w-[330px] sm:w-[370px]">
           <div className="w-full flex justify-center -mb-9">
             <Icons.camera height={100} width={70}/>
--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -155,5 +155,15 @@ export const Icons = {
                 </clipPath>
             </defs>
         </svg>
-    )
+    ),
+    chevronLeft: ({color, ..._props}: SVGProps<SVGSVGElement>) => (
+        <svg {..._props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke={color || "#0F172A"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M15 18l-6-6 6-6" />
+        </svg>
+    ),
+    chevronRight: ({color, ..._props}: SVGProps<SVGSVGElement>) => (
+        <svg {..._props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke={color || "#0F172A"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M9 18l6-6-6-6" />
+        </svg>
+    ),
 }
