b7fb204bb2f35504035200182636c21e774fd1d3
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-03 16:24:56 -0400

    fix: fixed not a jsx component issue

--- src/components/full-nav.tsx
+++ src/components/full-nav.tsx
@@ -1,16 +1,12 @@
 import Link from 'next/link';
 import React from 'react'
 import { Icons } from './ui/icons';
-import { getCurrentUser } from '~/lib/session';
 
-export default async function FullNav({children}: {children: React.ReactNode}) {
-
-  const user = await getCurrentUser()
-
+export default function FullNav({children}: {children: React.ReactNode}) {
   return (
     <header className='w-screen z-40 px-4 md:px-8'> 
       <div className='w-full max-w-[1400px] mx-auto flex justify-between items-center h-20 py-6'>
-        <Link href={!user ? '/' : '/calls'}>
+        <Link href={'/'}>
           <Icons.logo width="150" height="30"/>
         </Link>
         <nav className='text-neutral-700 font-normal'>
