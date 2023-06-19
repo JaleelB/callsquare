b832026d5cc8ccba7d5704c4f175fe830095d61e
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-18 23:29:14 -0400

    feat: programmatically creating room session

--- src/app/api/call/create/route.ts
+++ src/app/api/call/create/route.ts
@@ -5,12 +5,18 @@ import { authOptions } from "~/server/auth"
 import { prisma } from "~/server/db"
 
 const callCreateSchema = z.object({
-    id: z.string().uuid(),
+    name: z.string().uuid(),
+    audio: z.boolean(),
+    video: z.boolean(),
 })
 
 interface CallCreateBody {
-    id: string;
+    name: string;
 }
+
+type Room = {
+    id: string;
+};
 
 export async function POST(req: Request) {
 
@@ -29,9 +35,19 @@ export async function POST(req: Request) {
         const json: CallCreateBody = await req.json() as CallCreateBody;
         const body = callCreateSchema.parse(json)
 
+        const roomId = await createRoom(body.name);
+        const existingCall = await prisma.call.findUnique({
+            where: { id: roomId },
+        });
+          
+        if (existingCall) {
+            throw new Error('A call with this ID already exists');
+        }
+        
         const newCall = await prisma.call.create({
             data: {
-                id: body.id,
+                id: roomId,
+                name: body.name,
                 userId: user.id,
                 title: user.name + "'s Call",
                 startTime: new Date(),
@@ -39,7 +55,7 @@ export async function POST(req: Request) {
                 endTime: null,
             },
         });
-        
+          
         if (!newCall) {
             throw new Error('Error creating call');
         }
@@ -47,7 +63,7 @@ export async function POST(req: Request) {
         const inviteLink = `${env.NEXT_PUBLIC_APP_URL}/call/${newCall.id}`;
 
         // Update the call with the invite link
-        const updatedCall = await prisma.call.update({
+        await prisma.call.update({
             where: { id: newCall.id },
             data: { inviteLink },
         });
@@ -61,13 +77,37 @@ export async function POST(req: Request) {
                 role: 'host',
                 status: 'joined',
                 callId: newCall.id,
+                startTime: new Date(),
             },
         });
     
-        return new Response(JSON.stringify(updatedCall))
+        return new Response(JSON.stringify({ success: true }));
 
     } catch (error) {
-      return new Response(null, { status: 500 })
+        console.log(error)
+        return new Response(null, { status: 500 })
     }
 
-  }
+}
+
+async function createRoom(name: string){
+    const response = await fetch(`${env.TOKEN_ENDPOINT}/rooms`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${env.AUTH_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        template_id: env.TEMPLATE_ID,
+        region: 'us'
+      })
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const { id }: Room = await response.json() as Room;
+    return id;
+}
