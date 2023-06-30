7eb99c92687aab4077c459d4c8c84a34ad3bc361
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-30 00:47:10 -0400

    feat: redirect logo link to calls home when authenticated

--- src/components/full-nav.tsx
+++ src/components/full-nav.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
 import React from 'react'
 import { Icons } from './ui/icons';
+import { getCurrentUser } from '~/lib/session';
 
-export default function FullNav({children}: {children: React.ReactNode}) {
+export default async function FullNav({children}: {children: React.ReactNode}) {
+
+  const user = await getCurrentUser()
+
   return (
     <header className='w-screen px-4 md:px-8'> 
       <div className='w-full max-w-[1400px] mx-auto flex justify-between items-center h-20 py-6'>
-        <Link href={'/'}>
+        <Link href={`${user ? '/calls' : '/'}`}>
           <Icons.logo width="150" height="30"/>
         </Link>
         <nav className='text-neutral-700 font-normal'>
