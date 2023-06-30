84e34a9c9336b1b9390f75228e2f6d0d0d00430b
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-30 01:10:20 -0400

    feat: display call date & last 3o calls

--- src/app/calls/history/page.tsx
+++ src/app/calls/history/page.tsx
@@ -20,18 +20,31 @@ export default async function HistoryPage(){
         orderBy: {
             startTime: 'desc',
         },
+        take: 30,
     });
     
     return (
         <div className="container max-w-5xl mb-8 md:mb-12 lg:mb-16 mx-auto px-4 md:px-8">
-            <h1 className="text-3xl text-slate-900 md:text-4xl lg:text-5xl font-bold leading-tight mb-6 md:mb-16">Call History</h1>
+            <h1 className="text-3xl text-slate-900 md:text-4xl lg:text-5xl font-bold leading-tight mb-6 md:mb-10 lg:mb-16">Call History</h1>
             <div className="flex flex-col gap-2 md:gap-4">
-                {calls.map((call, index) => (
-                    <div key={index} className="flex gap-12 sm:gap-0 justify-between pb-4 border-b">
-                        <h2 className="truncate">{call.name}</h2>
-                        <p>{new Date(call.startTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-                    </div>
-                ))}
+                {
+                    calls.length !== 0 ? (
+                        calls.map((call, index) => (
+                            <div key={index} className="flex gap-12 sm:gap-0 justify-between pb-4 border-b">
+                                <h2 className="truncate">{call.name}</h2>
+                                <div>
+                                    <div className="hidden sm:flex gap-6">
+                                        <p>{new Date(call.startTime).toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: '2-digit' })}</p>
+                                        <p>{new Date(call.startTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                                    </div>
+                                    <p className="sm:hidden">{new Date(call.startTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-slate-900 text-center">No calls yet</p>
+                    )
+                }
             </div>
         </div>
     )
