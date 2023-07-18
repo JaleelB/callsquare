af87d72c0b3fe48c8033a10a8177c9cda7619d19
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-17 23:44:46 -0400

    feat: skeleton loading call history

--- src/app/(calls)/calls/history/loading.tsx
+++ src/app/(calls)/calls/history/loading.tsx
@@ -3,8 +3,12 @@ export default function CallHistoryLoading() {
         <div className="container max-w-[1400px] mb-12 mx-auto">
             <h1 className="text-2xl md:text-[30px] font-semibold leading-tight mb-1">Call history</h1>
             <p className="text-muted-foreground mb-8">Review your past interactions and revisit meaningful moments</p>
-            <div className="flex flex-col items-center justify-center h-full">
-                <p className="text-gray-500">Loading...</p>
+            
+            <div className="rounded-md border p-4">
+                <div className="h-4 bg-slate-100 animate-pulse w-1/3 mb-6"></div>
+                <div className="h-4 bg-slate-100 animate-pulse w-full my-2"></div>
+                <div className="h-4 bg-slate-100 animate-pulse w-full my-2"></div>
+                <div className="h-10 bg-slate-100 animate-pulse w-1/4 mt-12"></div>
             </div>
         </div>
     )
