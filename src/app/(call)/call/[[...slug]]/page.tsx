1d416c10215d262ad28a85cb27021f2b2f3a57a1
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-19 21:18:56 -0400

    refactor: improve call page logic

--- src/app/(call)/call/[[...slug]]/page.tsx
+++ src/app/(call)/call/[[...slug]]/page.tsx
@@ -23,100 +23,99 @@ export default function CallPage(){
     const roomId = Cookies.get("room-id");
     const unAuthUsername = Cookies.get("username");
 
-    React.useEffect(() => {
+    const joinCall = React.useCallback(async () => {
 
-        async function joinCall(){
-            
-            if (!roomId) {
-                console.error("Room id is not defined");
-                return;
-            }
-
-            try {
-
-                const roomCodeResponse = await fetch(`/api/call/code`, {
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                      callName: roomName ? roomName : params.slug,
-                    }),
-                })
-
-                if(roomCodeResponse?.ok){
-    
-                 // use room code to fetch auth token
-                    const codeResponse: RoomCodeResponse = await roomCodeResponse.json() as RoomCodeResponse;
-                    const roomCode = codeResponse.code;
-                    const authToken = await hmsActions.getAuthTokenByRoomCode({ roomCode })
-                    const session = await getSession();
-
-                    if(session && session.user.name){
-                        const userName = session.user.name;
-                        await hmsActions.join({ userName, authToken });
-                    } else if(!session && unAuthUsername){
-                        await hmsActions.join({ userName: unAuthUsername, authToken });
-                    }
-                    else {
-                        console.error("Session or user name is not defined");
-                        toast({
-                            title: "Something went wrong.",
-                            description: "This call cannot joined. Please try again.",
-                            variant: "destructive",
-                        });
-                        router.replace("/calls");
-                    }
-                } else {
-                    throw new Error("Room code response not OK");
-                }
-
-            } catch (error) {
-                console.error(error)
-                toast({
-                    title: "Something went wrong.",
-                    description: "This call cannot be joined. Please try again.",
-                    variant: "destructive",
-                })
-                router.replace("/calls");
-            }
+        if (!roomId) {
+            console.error("Room id is not defined");
+            return;
         }
-        void joinCall();
+
+        try {
+            const roomCodeResponse = await fetch(`/api/call/code`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  callName: roomName ? roomName : params.slug,
+                }),
+            })
+
+            if(roomCodeResponse?.ok){
+
+             // use room code to fetch auth token
+                const codeResponse: RoomCodeResponse = await roomCodeResponse.json() as RoomCodeResponse;
+                const roomCode = codeResponse.code;
+                const authToken = await hmsActions.getAuthTokenByRoomCode({ roomCode })
+                const session = await getSession();
+
+                if(session && session.user.name){
+                    const userName = session.user.name;
+                    await hmsActions.join({ userName, authToken });
+                } else if(!session && unAuthUsername){
+                    await hmsActions.join({ userName: unAuthUsername, authToken });
+                }
+                else {
+                    console.error("Session or user name is not defined");
+                    toast({
+                        title: "Something went wrong.",
+                        description: "This call cannot joined. Please try again.",
+                        variant: "destructive",
+                    });
+                    router.replace("/calls");
+                }
+            } else {
+                throw new Error("Room code response not OK");
+            }
+
+        } catch (error) {
+            console.error(error)
+            toast({
+                title: "Something went wrong.",
+                description: "This call cannot be joined. Please try again.",
+                variant: "destructive",
+            })
+            router.replace("/calls");
+        }
 
     }, [hmsActions, toast, params.slug, router, roomName, roomId, unAuthUsername]);
+
+    const leaveCall = React.useCallback(async () => {
+        
+        const response = await fetch(`/api/call/leave`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              callName: roomName ? roomName : extractId(params.slug as string),
+              roomId: roomId,
+            }),
+        })
+      
+        if(!response.ok){
+              toast({
+                  title: "Something went wrong.",
+                  description: "Your call cannot be left. Please try again.",
+                  variant: "destructive",
+              })
+        } 
+          
+        await actions.leave();
+    }, [roomName, params.slug, roomId, actions, toast]);
+
+    React.useEffect(() => {
+        void joinCall();
+    }, [joinCall]);
 
     React.useEffect(() => {
         window.onunload = () => {
             if (isConnected) {
-
-                async function leaveCall() {
-                    const response = await fetch(`/api/call/leave`, {
-                      method: "PATCH",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({
-                        callName: roomName ? roomName : extractId(params.slug as string),
-                        roomId: roomId,
-                      }),
-                    })
-                
-                    if(!response.ok){
-                        toast({
-                            title: "Something went wrong.",
-                            description: "Your call cannot be left. Please try again.",
-                            variant: "destructive",
-                        })
-                    } 
-                    
-                    await actions.leave();
-                }
-
                 void leaveCall();
             }
         };
 
-    }, [actions, toast, hmsActions, isConnected, params.slug, roomId, roomName, router]);
+    }, [isConnected, leaveCall]);
 
 
     return(
