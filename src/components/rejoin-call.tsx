faa3aac667bacb1485a9279163e4a2c8eb330ec9
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-08-20 15:11:15 -0400

    feat: add countdown timer for auto-return to homescreen after call exit

--- src/components/rejoin-call.tsx
+++ src/components/rejoin-call.tsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 import { useRouter } from 'next/navigation';
 import { useToast } from "./ui/use-toast";
 import { useHMSActions } from "@100mslive/react-sdk";
+import * as React from 'react';
 
 export default  function RejoinCall({
     roomName,
@@ -17,9 +18,9 @@ export default  function RejoinCall({
     const router = useRouter();
     const { toast } = useToast();
     const actions = useHMSActions();
+    const [secondsLeft, setSecondsLeft] = React.useState(60); // 10 seconds countdown
 
-    async function leaveCall() {
-    
+    const leaveCall = React.useCallback(async () => {
         const response = await fetch(`/api/call/leave`, {
           method: "PATCH",
           headers: {
@@ -29,22 +30,59 @@ export default  function RejoinCall({
             callName: roomName,
             roomId: roomId
           }),
-        })
+        });
     
         if(!response.ok){
           toast({
             title: "Something went wrong.",
             description: "Your call cannot be left. Please try again.",
             variant: "destructive",
-          })
+          });
         } 
-
+    
         await actions.leave();
-        
-    }
+
+    }, [roomName, roomId, toast, actions]);
+    
+
+    React.useEffect(() => {
+        if (secondsLeft > 0) {
+            const timerId = setTimeout(() => {
+                setSecondsLeft(prevSeconds => prevSeconds - 1);
+            }, 1000);
+            return () => clearTimeout(timerId);
+        } else {
+            void leaveCall();
+            router.replace("/calls");
+        }
+    }, [leaveCall, router, secondsLeft]);    
+
+    const strokeDashoffset = ((60 - secondsLeft) / 60) * 283;
 
     return (
         <section className="fixed inset-0 w-screen h-screen bg-white flex items-center justify-center z-40">
+            <div className="absolute top-6 left-6 flex gap-4 items-center">
+                <div className="relative w-12 h-12 flex gap-2">
+                    <svg width="100%" height="100%" viewBox="0 0 100 100">
+                        <circle cx="50" cy="50" r="45" fill="none" stroke="#ddd" strokeWidth="5" />
+                        <circle 
+                            cx="50" 
+                            cy="50" 
+                            r="45" 
+                            fill="none" 
+                            stroke="#10172A" 
+                            strokeWidth="5" 
+                            strokeLinecap="round"
+                            strokeDasharray="283"
+                            strokeDashoffset={strokeDashoffset}
+                        />
+                    </svg>
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <span className="text-primary text-xs">{secondsLeft}s</span>
+                    </div>
+                </div>
+                <span className="text-primary text-sm">Returning to the homescreen</span>
+            </div>
             <div className="flex flex-col gap-8">
                 <div>
                     <h2 className="text-primary text-3xl">You have left the call</h2>
