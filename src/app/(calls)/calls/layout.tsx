c247f45330bd1b357b2c1506e7495c2873e62096
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-11-06 23:05:39 -0500

    fix: unused '@ts-expect-error' directive error

--- src/app/(calls)/calls/layout.tsx
+++ src/app/(calls)/calls/layout.tsx
@@ -2,39 +2,35 @@ import SiteFooter from "~/components/layout/footer";
 import FullNav from "~/components/layout/full-nav";
 import UserAccountDropdown from "~/components/layout/user-account-dropdown";
 import { getCurrentUser } from "~/lib/session";
-import { notFound } from "next/navigation"
+import { notFound } from "next/navigation";
 import CallIdProvider from "~/context/call-id-context";
 
 export default async function CallsHomeLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-
-  const user = await getCurrentUser()
+  const user = await getCurrentUser();
 
   if (!user) {
-    notFound()
+    notFound();
   }
 
   return (
     <div className="flex min-h-screen flex-col">
-      {/* @ts-expect-error Server Component */}
       <FullNav>
-        <UserAccountDropdown 
-          user={{ 
-            name: user?.name || '', 
-            email: user?.email || '',
-            image: user?.image || '' 
-          }} 
+        <UserAccountDropdown
+          user={{
+            name: user?.name || "",
+            email: user?.email || "",
+            image: user?.image || "",
+          }}
         />
       </FullNav>
-      <main className="flex-1 w-screen my-16 md:my-8 flex items-center">
-        <CallIdProvider>
-          {children}
-        </CallIdProvider>
+      <main className="my-16 flex w-screen flex-1 items-center md:my-8">
+        <CallIdProvider>{children}</CallIdProvider>
       </main>
-      <SiteFooter/>
+      <SiteFooter />
     </div>
   );
 }
