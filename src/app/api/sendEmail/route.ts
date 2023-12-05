f911cd8666978165f5730d72a32a07ced86b98b4
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-12-04 21:07:35 -0500

    refactor: email endpoint logic

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,57 +1,57 @@
-import { Resend } from 'resend';
-import { env } from '~/env.mjs';
-import { type EmailProps } from '~/types/types';
-import { z } from "zod"
-import { InviteEmail } from '~/components/email-template';
-import { type ErrorResponse } from "resend"
+import { Resend } from "resend";
+import { env } from "~/env.mjs";
+import { type EmailProps } from "~/types/types";
+import { z } from "zod";
+import { InviteEmail } from "~/components/email-template";
+import { type ErrorResponse } from "resend";
 
 const resend = new Resend(env.RESEND_API_KEY);
 
 const emailSchema = z.object({
-    recipient: z.string(),
-    link: z.string(),
-    recipientUsername: z.string(),
-    senderImage: z.string(),
-    invitedByUsername: z.string(),
-    invitedByEmail: z.string(),
+  recipient: z.string(),
+  link: z.string(),
+  recipientUsername: z.string(),
+  senderImage: z.string(),
+  invitedByUsername: z.string(),
+  invitedByEmail: z.string(),
 });
 
 export async function POST(req: Request) {
+  const json: EmailProps = (await req.json()) as EmailProps;
+  const body = emailSchema.parse(json);
 
-    const json: EmailProps = await req.json() as EmailProps;
-    const body = emailSchema.parse(json);
-    
-    try {
+  try {
+    const { error } = await resend.emails.send({
+      from: `Callsquare <${body.invitedByEmail}>`,
+      to: body.recipient,
+      subject: "Invitation to join call on Callsquare",
+      react: InviteEmail({
+        recipientUsername: body.recipientUsername,
+        senderImage: body.senderImage,
+        invitedByUsername: body.invitedByUsername,
+        invitedByEmail: body.invitedByEmail,
+        inviteLink: body.link,
+      }),
+      text: "Invitation to join call on Callsquare",
+    });
 
-        await resend.sendEmail({
-            from: `Callsquare <${body.invitedByEmail}>`,
-            to: body.recipient,
-            subject: 'Invitation to join call on Callsquare',
-            react: (
-                InviteEmail({ 
-                    recipientUsername: body.recipientUsername,
-                    senderImage: body.senderImage,
-                    invitedByUsername: body.invitedByUsername,
-                    invitedByEmail: body.invitedByEmail,
-                    inviteLink: body.link
-                })
-            ),
-        });
-
-        return new Response(null, { status: 200 })
-        
-    } catch (error) {
-
-        const resendError = error as ErrorResponse
-
-        if (resendError?.error?.message) {
-        return new Response(resendError.error.message, { status: 429 })
-        }
-
-        if (error instanceof Error) {
-        return new Response(error.message, { status: 500 })
-        }
-
-        return new Response("Something went wrong", { status: 500 })
+    if (error) {
+      console.log(error);
+      return new Response(error.message, { status: 500 });
     }
+
+    return new Response(null, { status: 200 });
+  } catch (error) {
+    const resendError = error as ErrorResponse;
+
+    if (resendError?.message) {
+      return new Response(resendError?.message, { status: 429 });
+    }
+
+    if (error instanceof Error) {
+      return new Response(error.message, { status: 500 });
+    }
+
+    return new Response("Something went wrong", { status: 500 });
+  }
 }
