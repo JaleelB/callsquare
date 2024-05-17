5cf38979b06ac94d7a964af240b803eaf0b48c34
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-05-16 21:58:48 -0400

    fix: remove ts-expect-error server component annotation

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,38 +4,35 @@ import FullNav from "~/components/layout/full-nav";
 import { Button } from "~/components/ui/button";
 
 export default function HomePageLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <div className="flex min-h-screen flex-col">
-        {/* @ts-expect-error Server Component */}
-        <FullNav>
-          <div className='flex gap-x-1.5'>
-            <Link  href={"/login"}>
-              <Button 
-                className="mr-2 rounded-md font-normal hidden sm:block" 
-                variant='ghost'
-                size="sm"
-              >
-                Sign in
-              </Button>
-            </Link>
-             <Link  href={"/register"}>
-              <Button 
-                className="rounded-md text-xs md:text-[12px] font-normal"
-                size="sm"
-              >
-                Try Callsquare
-                </Button>
-              </Link>
-          </div>
-        </FullNav>
-        <main className="flex-1 w-screen">
-            {children}
-        </main>
-        <SiteFooter/>
+      <FullNav>
+        <div className="flex gap-x-1.5">
+          <Link href={"/login"}>
+            <Button
+              className="mr-2 hidden rounded-md font-normal sm:block"
+              variant="ghost"
+              size="sm"
+            >
+              Sign in
+            </Button>
+          </Link>
+          <Link href={"/register"}>
+            <Button
+              className="rounded-md text-xs font-normal md:text-[12px]"
+              size="sm"
+            >
+              Try Callsquare
+            </Button>
+          </Link>
+        </div>
+      </FullNav>
+      <main className="w-screen flex-1">{children}</main>
+      <SiteFooter />
     </div>
   );
 }
