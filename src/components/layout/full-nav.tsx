23a6cea0951e45c1dd625a3d46ab532b753a2058
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-05-17 09:35:49 -0400

    feat: added theme toggle to footer

--- src/components/layout/full-nav.tsx
+++ src/components/layout/full-nav.tsx
@@ -14,9 +14,9 @@ export default async function FullNav({
     <header className="w-screen">
       <div className="container mx-auto flex h-20 max-w-[1400px] items-center justify-between py-6">
         <Link href={`${user ? "/calls" : "/"}`}>
-          <Icons.logo width="150" height="30" />
+          <Icons.logo width="150" height="30" className="text-primary" />
         </Link>
-        <nav className="font-normal text-neutral-700">{children}</nav>
+        <nav className="font-normal text-primary">{children}</nav>
       </div>
     </header>
   );
